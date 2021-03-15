4d71c8cdc60584472bead6c564afc99219be77bf
Ankush Daharwal <59690435+AnkushDaharwal07@users.noreply.github.com>
2021-03-16 00:09:50 +0530

    Update OrderRepository.java

--- OrderMS/src/main/java/ekart/order/repository/OrderRepository.java
+++ OrderMS/src/main/java/ekart/order/repository/OrderRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ekart.order.entity.Order;
 
 public interface OrderRepository extends JpaRepository<Order,Integer> {
-    List<ProductsOrdered> findByOrderId(int ORDERID);
+    List<Order> findByBuyerId (int buyerId);
 }
